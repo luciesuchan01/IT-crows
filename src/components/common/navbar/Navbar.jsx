49b2e34c17a8fface9dbd28ac3fb411418423067
luciesuchn01 <lucie.suchankova2@seznam.cz>
2022-10-31 14:51:53 +0100

    z8kladni struktura header - logo, navbar - nenaformatovane

---
+++ src/components/common/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Navbar.css'
+function Navbar(){
+
+    return(
+        <section>
+            <nav>
+                <ul>
+                    <li>en</li>
+                    <li>our work</li>
+                    <li>services</li>
+                    <li>company</li>
+                    <li>start project</li>
+
+            </ul>
+            </nav>
+        </section>
+    )
+
+}
+
+export default Navbar;
