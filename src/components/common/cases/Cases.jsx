fc9a8eaf143330076d539dd60da86d2149931078
luciesuchn01 <lucie.suchankova2@seznam.cz>
2022-11-08 19:56:24 +0100

    upraveno rozložení + odkaz view more přeformátován

--- src/components/common/cases/Cases.jsx
+++ src/components/common/cases/Cases.jsx
@@ -16,60 +16,68 @@ function Cases(){
             </section>
             <div class="grid-container">
                 <div class="grid-item">
-                    <img src={graciano_picture} alt="" width="650" height="600"/>
-                   
-                    <h1>graciano
-                    <h2>
-                        <div className='first-pic'>UI/UIX, develop, e-commerce</div>
-                    </h2>
-                    </h1>
+                    <div className='grid-item-left'>
+                        <img src={graciano_picture} alt="" width="550" height="500"/>
+                    
+                        <h1>graciano
+                        <h2>
+                            <div className='first-pic'>UI/UIX, develop, e-commerce</div>
+                        </h2>
+                        </h1>
+                    </div>
                 </div>                
                 <div class="grid-item">
-                    <div class = "pujc-border">
-                        <div className='pujc-pic'>
-                            <img src={pujc_picture} alt="" width="250" height="550"/>
+                    <div class="grid-item-left">
+                        <div class = "pujc-border">
+                            <div className='pujc-pic'>
+                                <img src={pujc_picture} alt="" width="200" height="480"/>
+                            </div>
                         </div>
+                        <h1>pujc
+                        <h2>
+                            <div className='second-pic'>UI/UX, develop</div>
+                        </h2>
+                        </h1>
                     </div>
-                    <h1>pujc
-                    <h2>
-                        <div className='second-pic'>UI/UX, develop</div>
-                    </h2>
-                    </h1>
                 </div>
                
 
                 <div class="grid-item">
-                    <img src={todolist_picture} alt="" width="550" height="600"/>
-                    <h1>todo list
-                        <h2>
-                            <div className='third-pic'>UI/UX, develop, app</div>
-                        </h2>
-                    </h1>
+                    <div className='grid-item-left'>
+                        <img src={todolist_picture} alt="" width="450" height="450"/>
+                    
+                        <h1>todo list
+                            <h2>
+                                <div className='third-pic'>UI/UX, develop, app</div>
+                            </h2>
+                        </h1>
+                    </div>
 
                 </div>
 
                 <div class="grid-item">
-                    <img src={lorem_picture} alt="" width="400" height="400"/>
+                    <div class="grid-item-left">
+                    <img src={lorem_picture} alt="" width="350" height="350"/>
                     <h1>lorem
                         <h2>
                             <div className='fourth-pic'>design, UI/UX</div>
                         </h2>
                     </h1>
-
+                    </div>
                 </div>
             </div>
             <div className='grid-container-second'>
-                <div class="grid-item">
-                    <img src={ecommerce_picture} alt="" width="1050" height="500"/>
+                <div class="grid-item-left">
+                    <img src={ecommerce_picture} alt="" width="950" height="500"/>
+                
                     <h1>lorem
                         <h2>
                             <div className='fifth-pic'>design, UI/UX, develop, e-commerce</div>
                         </h2>
                     </h1>
-
                 </div>
             </div>
-            <a href='/'>
+            <a href='/view-more' className='more-view-a'>
 
                 <div className='more-view'>View more</div>
                     <img src={sipka_picture} alt="" width="133" height="24"/>
