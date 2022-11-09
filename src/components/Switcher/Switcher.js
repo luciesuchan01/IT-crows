c8c73732e1ec60b720d629474674ab1e2dd07b54
J.Gross <jan.gross.cze@gmail.com>
2022-11-09 16:14:12 +0100

    Doplněn nadpis, toggle button, project button. Chybi responzivita. U toggle buttonu chybí akce

--- src/components/Switcher/Switcher.js
+++ src/components/Switcher/Switcher.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import './Switcher.css';
+import Arrow from './images/right_arrow.svg';
+
 
 export default function Switcher() {
     return(
-        <div>
+        <div className="Switcher">
+            <h2 className="Switcher-title">
+                Switch 
+
+        <span className="Span-label">
+                <label className="Switch-label">
+                    <input type="checkbox" className="Switch-input" />
+                    <span className="slider round"></span>
+                </label>
+        </span>
+
+                
+                
+                
+                 your <span className="Switcher-red-text"> business </span>  <br />
+                to the <span className="Switcher-red-text"> next level </span> 
+            </h2>
+
+           
+
+            <div className="Switcher-project-btn">
+                <a href="#">
+                    Discus a project 
+                </a>
+                <img src={Arrow} className="Arrow" />
+            </div>
             
         </div>
     )
