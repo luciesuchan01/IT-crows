3bf9cf1bc9c3c2bf404ed83d6e0febb72a93cb94
luciesuchn01 <lucie.suchankova2@seznam.cz>
2022-12-09 21:35:26 +0100

    Contact - Let's talk, buttons, form, todo-footer

--- src/components/common/contact/Form.jsx
+++ src/components/common/contact/Form.jsx
@@ -8,9 +8,49 @@ function Form(){
             <div className='help-title'>
                 how can we help you with?
             </div>
-            {/*<button type="button" >Click Me</button>*/}
-            
+            <div className='buttons-first-row'>
+                <button type="submit" className='grey-button-first'>Web apps</button> 
+                <button type="submit" className='red-button-first'>Custom software</button> 
+                <button type="submit" className='grey-button-second'>Website</button> 
+                <button type="submit" className='grey-button-third'>AI</button> 
+                <button type="submit" className='grey-button-fourth'>UI/UIX</button> 
+                <button type="submit" className='red-button-second'>Branding</button> 
+                <button type="submit" className='grey-button-fifth'>Webflow</button> 
+            </div>
+             
 
+
+            <div class="form-container">
+                <div className='form-title'>A BIT ABOUT YOU AND PROJECT</div>
+                    <form class="register-form">
+                        <input
+                        id="first-name"
+                        class="form-field"
+                        type="text"
+                        placeholder="First Name"
+                        name="firstName"
+                        />
+                        <input
+                        id="email"
+                        class="form-field"
+                        type="text"
+                        placeholder="E-mail"
+                        name="email"
+                        />
+                        <input
+                        id="details"
+                        class="form-field"
+                        type="text"
+                        placeholder="Project details (optional)"
+                        name="details"
+                        />
+                        
+                        <button class="form-btn" type="submit">
+                        Start a project
+                        </button>
+                    </form>
+            </div>
+        
 
         </section>
     )
