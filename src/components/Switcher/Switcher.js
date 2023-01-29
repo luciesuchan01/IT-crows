9870e134eb4872afc570e482aed057691bf206ba
luciesuchn01 <lucie.suchankova2@seznam.cz>
2023-01-29 14:23:52 +0100

    About us, Switcher - nefunkcni objevovani tlacitka

--- src/components/Switcher/Switcher.js
+++ src/components/Switcher/Switcher.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import './Switcher.css';
 import Arrow from './images/right_arrow.svg';
-/*
-const toggleBtn = document.querySelector('#toggleBtn');
-    const divList = document.querySelector("Switcher-project-btn");
 
-    toggleBtn.addEventListener('click', () => {
-        if(divList.style.display == "none"){
-            divList.style.display = "block";
-        }else{
-            divList.style.display = "none";
-        }
-    });
-*/
 export default function Switcher() {
+    
     return(
+        
         <div className="Switcher">
             <h2 className="Switcher-title">
                 Switch 
 
         <span className="Span-label">
                 <label className="Switch-label">
-                    <input type="checkbox" className="Switch-input" id = "#toggleBtn"/>
+                    <input type="checkbox" className="Switch-input" value={1}/>
                     <span className="slider round"></span>
                 </label>
         </span>
@@ -29,13 +20,9 @@ export default function Switcher() {
                  your <span className="Switcher-red-text"> business </span>  <br />
                 to the <span className="Switcher-red-text"> next level </span> 
             </h2>
-            <a href="/contact">
 
-            <div className="Switcher-project-btn" >
-                    Discuss a project 
-                <img src={Arrow} className="Arrow" />
-            </div>
-            
+            <a href="/contact">
+                <input type="submit" className="Switcher-project-btn" value="Discuss a project"/>
             </a>
 
         </div>
